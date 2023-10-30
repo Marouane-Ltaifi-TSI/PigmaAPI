6ded37cf896c44859863aabc34f146da2c27d419
Marouane-Ltaifi-TSI <marouane.ltafi@tsi.com.tn>
2023-10-30 15:30:42 +0100

    add token bearer to swagger ui

--- PigmaAPI/Program.cs
+++ PigmaAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PigmaAPI.Helpers;
 using PigmaAPI.Infrastructure.ApplicationDbContext;
 using PigmaAPI.Services.ServiceInjections;
@@ -26,7 +27,34 @@ builder.Services.AddCors(
              });
     });
 builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
 
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
 options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
